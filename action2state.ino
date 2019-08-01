5547e0900ab4d97fa7db3cd1c433185f302759c4
dschaele <48531705+dschaele@users.noreply.github.com>
2019-08-01 17:24:07 +0200

    Updated action2state.ino (Compiles)

--- action2state.ino
+++ action2state.ino
@@ -1,28 +1,60 @@
-int action2state(int &state1, int &state2, int n_states[2], int action) //n_states is the number of states for each servo
+void action2state(int gwS1, int gwS2, int& newgwS1, int& newgwS2, int n_states[2], int action) //n_states is the number of states for each servo
 {
-  int state[2] = {state1, state2};
-  int newstate[2] = {0, 0};
-  byte i = 0;
-  
-  for (i=0; i < 2; i++)
-    {newstate[i] = state[i];}
+   
+if (action == 0)
+    {
+      //Serial1.print("A0 ");
+    if ((gwS1 <= 0) && (action == 0))
+      {
+      newgwS1 = gwS1;
+      newgwS2 = gwS2;
+      }
+    else 
+      {
+      newgwS1 = gwS1 - 1;
+      newgwS2 = gwS2;
+      }
+    }
     
-  if ((state[0] == 1 && action == 0)||(state[0] == n_states[0] && action == 1)) //actions up=1; down=2;
-    {for (i=0; i < 2; i++)
-    {newstate[i] = state[i];}}
-  else if ((state[1] == 1 && action == 2)||(state[1] == n_states[1] && action == 3)) //actions backwards=3; forwards=4;
-    {for (i=0; i < 2; i++)
-    {newstate[i] = state[i];}}
-  else if (action == 0)
-    {newstate[0] = state[0] + 1;}
-  else if (action == 1)
-    {newstate[0] = state[0] - 1;}
-  else if (action == 2)
-    {newstate[1] = state[1] - 1;}
-  else if (action == 3)
-    {newstate[1] = state[1] + 1;}
-  else {for (i=0; i < 2; i++)
-    {newstate[i] = state[i];}}
+   if (action == 1)
+    { //Serial1.print("A1 ");
+    if ((gwS1 >= n_states[0]-1) && (action == 1))
+      {
+      newgwS1 = gwS1;
+      newgwS2 = gwS2;
+      }
+    else 
+      {
+      newgwS1 = gwS1 + 1;
+      newgwS2 = gwS2;
+      }
+    }
 
-  //return newstate;
+   if (action == 2)
+    { //Serial1.print("A2 ");
+    if ((gwS2 <= 0) && (action == 2))
+      {
+      newgwS1 = gwS1;
+      newgwS2 = gwS2;
+      }
+    else 
+      {
+      newgwS1 = gwS1;
+      newgwS2 = gwS2 - 1;
+      }
+    }
+
+   if (action == 3)
+    {// Serial1.print("A3 ");
+    if ((gwS2 >= n_states[1]-1)) //&& (action == 3))
+      {
+      newgwS1 = gwS1;
+      newgwS2 = gwS2;
+      }
+    else
+      { 
+      newgwS1 = gwS1;  
+      newgwS2 = gwS2 + 1;
+      }
+    }
 } 
--- myFcns.h
+++ myFcns.h
@@ -1,5 +1,5 @@
 //Epsilon greedy
-int e_greedy(float &e_greed, float e_greed_final, float e_greed_step, float Q[4]);
+//int e_greedy(float &e_greed, float e_greed_final, float e_greed_step, float Q[4]);
 
 //Delta: Mapping from actions to states
 void action2state(int gwS1, int gwS2, int& newgwS1, int& newgwS2, int n_states[2], int action);
